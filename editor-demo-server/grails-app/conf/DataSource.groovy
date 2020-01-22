bfa1f99204ce7f8b7702ab5bbe3daf2384d17043
Dmitry Divin <divin@scand.com>
2020-01-22 09:42:41 +0300

    Fixing databes connection and remove editor

--- editor-demo-server/grails-app/conf/DataSource.groovy
+++ editor-demo-server/grails-app/conf/DataSource.groovy
@@ -1,24 +1,55 @@
 dataSource {
-    pooled = false
-    driverClassName = "com.mysql.jdbc.Driver"
-    dbCreate = "create-drop"
-    url = "jdbc:mysql://localhost:3306/test"
-    username = "root"
-    password = "root"
-    dialect = 'com.jcatalog.util.database.CustomMySQL5InnoDBDialect'
+    pooled = true
+    jmxExport = true
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
 }
-//dataSource {
-//    pooled = false
-//    driverClassName = "org.h2.Driver"
-//    dbCreate = "create-drop"
-//    url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//    username = "sa"
-//    password = ""
-//    dialect = 'org.hibernate.dialect.H2Dialect'
-//}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
     naming_strategy = com.jcatalog.util.database.hibernate.ImprovedNamingStrategyWithAnnotations
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            properties {
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+        }
+    }
 }
