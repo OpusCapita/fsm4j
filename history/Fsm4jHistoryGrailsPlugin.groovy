5c909fbcf468399d03f59b46bf0e13f071813c6c
ocmachineuser <29400948+ocmachineuser@users.noreply.github.com>
2020-03-10 08:10:10 +0300

    Advancing project version to the next development - 1.0.3-SNAPSHOT.

--- history/Fsm4jHistoryGrailsPlugin.groovy
+++ history/Fsm4jHistoryGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class Fsm4jHistoryGrailsPlugin {
-    def version = "1.0.2"
+    def version = "1.0.3-SNAPSHOT"
     def groupId = "com.opuscapita.grailsplugins"
     def grailsVersion = "2.4 > *"
 
